5ca5bcae7015b2eb70788b6b1b67e3b2f003c6c3
Stephen Cassidy <scassid6@jhu.edu>
2023-01-26 17:01:25 -0500

    move files to better place for artifacts and add gulp jobs to zip job

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 const { src, dest } = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const clean = require('gulp-clean');
+
+function cleanFiles () {
+    return src(['01JHU_INST-JHU/js/custom.js', '01JHU_INST-JHU/css/custom1.css'], {read: false})
+        .pipe(clean());
+}
 
 function scripts () {
   return src('01JHU_INST-JHU/js/*.js')
     .pipe(uglify())
     .pipe(concat('custom.js'))
-    .pipe(dest('01JHU_INST-JHU/vendor/js/'));
+    .pipe(dest('01JHU_INST-JHU/js/'));
 };
 
 function styles () {
     return src('01JHU_INST-JHU/css/*.css')
         .pipe(concat('custom1.css'))
-        .pipe(dest('01JHU_INST-JHU/vendor/css/'));
+        .pipe(dest('01JHU_INST-JHU/css/'));
 };
 
+exports.cleanFiles = cleanFiles;
 exports.scripts = scripts;
 exports.styles = styles;
