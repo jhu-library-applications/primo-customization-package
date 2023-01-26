f59e8a824129260e5d0e8ad66b8d7ccf51e88f32
Stephen Cassidy <scassid6@jhu.edu>
2023-01-26 17:10:21 -0500

    add check to see if file exists

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,22 @@ const { src, dest } = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const clean = require('gulp-clean');
+const fs = require('fs');
 
-function cleanFiles () {
-    return src(['01JHU_INST-JHU/js/custom.js', '01JHU_INST-JHU/css/custom1.css'], {read: false})
+function cleanJS () {
+  if(fs.existsSync('01JHU_INST-JHU/js/custom.js')) {
+    return src(['01JHU_INST-JHU/js/custom.js'], {read: false})
+    .pipe(clean());
+  }
+  return Promise.resolve('No JS to clean');
+}
+
+function cleanCSS () {
+  if(fs.existsSync('01JHU_INST-JHU/css/custom1.css')) {
+    return src(['01JHU_INST-JHU/css/custom1.css'], {read: false})
         .pipe(clean());
+  }
+  return Promise.resolve('No CSS to clean');
 }
 
 function scripts () {
@@ -21,6 +33,7 @@ function styles () {
         .pipe(dest('01JHU_INST-JHU/css/'));
 };
 
-exports.cleanFiles = cleanFiles;
+exports.cleanJS = cleanJS;
+exports.cleanCSS = cleanCSS;
 exports.scripts = scripts;
 exports.styles = styles;
