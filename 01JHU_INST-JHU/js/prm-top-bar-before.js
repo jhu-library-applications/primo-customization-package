e140a8e7bbd0e1b6dd4b1f1fecf2d6cba89ec11d
little9 <jamie@jamielittle.org>
2023-02-09 16:46:23 -0600

    Use the Primo VE /discovery/ path for assets

--- 01JHU_INST-JHU/js/prm-top-bar-before.js
+++ 01JHU_INST-JHU/js/prm-top-bar-before.js
@@ -1,9 +1,10 @@
 (function () {
     "use strict";
     'use strict';
+
 var app = angular.module('viewCustom', ['angularLoad']);
     app.component('jhuTopBar', {
-	templateUrl: "/primo-explore/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
+	templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
     });
 
 app.component('prmTopBarBefore', {
