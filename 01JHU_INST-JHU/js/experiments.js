70eb32dd0e2383c093ca0da6cdf54ebb36864bb8
Stephen Cassidy <scassid6@jhu.edu>
2023-02-09 15:49:08 -0500

    Try out some code to find out how to edit topbar

---
+++ 01JHU_INST-JHU/js/experiments.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+    'use strict';
+
+
+    var app = angular.module('viewCustom', ['angularLoad']);
+app.component('prmTopBar', {
+    template: `<span style="margin-left: 40%;">Hello World</span>`
+
+});
+    
+
+})();
