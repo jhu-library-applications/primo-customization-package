5353074502c7c0422b3bef8a3e64607c5b34da96
Jamie Little <jamie@jamielittle.org>
2023-02-10 09:02:37 -0600

    Update prm-top-bar-before.js

--- 01JHU_INST-JHU/js/prm-top-bar-before.js
+++ 01JHU_INST-JHU/js/prm-top-bar-before.js
@@ -3,13 +3,10 @@
     'use strict';
 
 var app = angular.module('viewCustom', ['angularLoad']);
-    app.component('jhuTopBar', {
-	templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
-    });
 
 app.component('prmTopBarBefore', {
         bindings: {parentCtrl: `<`},
-        template: `<jhu-top-bar></jhu-top-bar>`    
+        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"  
 });
 	
 })();
