f27b2d8325d178b6bc493206fbfc2a89b9f8c7c3
little9 <jamie@jamielittle.org>
2023-02-15 09:34:47 -0600

    Additional overrides

---
+++ 01JHU_INST-JHU/js/prm-back-to-library-search-button-after.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    var app = angular.module('viewCustom', ['angularLoad']);
+
+    app.component('prmBackToLibrarySearchButtonAfter', {
+        bindings: { parentCtrl: `<` },
+        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-back-to-library-search-button-after.html"
+    });
+})();
