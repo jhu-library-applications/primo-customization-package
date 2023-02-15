b99748fde94ced3b5c820a5d3ab624c5394961b5
little9 <jamie@jamielittle.org>
2023-02-15 09:44:59 -0600

    Remove function

--- 01JHU_INST-JHU/js/prm-top-bar-before.js
+++ 01JHU_INST-JHU/js/prm-top-bar-before.js
@@ -1,21 +1,16 @@
-(function () {
-    "use strict";
-
 var app = angular.module('viewCustom', ['angularLoad']);
 
 app.component('prmTopBarBefore', {
-        bindings: {parentCtrl: `<`},
-        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"  
+    bindings: { parentCtrl: `<` },
+    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
 });
 
 app.component('prmSearchBarAfter', {
-    bindings: {parentCtrl: `<`},
-    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"  
+    bindings: { parentCtrl: `<` },
+    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
 });
-	
+
 app.component('prmBackToLibrarySearchButtonAfter', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-back-to-library-search-button-after.html"
 });
-
-})();
