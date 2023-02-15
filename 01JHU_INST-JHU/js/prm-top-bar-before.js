54250db19bfb2edad6eb4ad4ffa6cc15c450d5d2
little9 <jamie@jamielittle.org>
2023-02-15 09:23:11 -0600

    Testing having one JS file

--- 01JHU_INST-JHU/js/prm-top-bar-before.js
+++ 01JHU_INST-JHU/js/prm-top-bar-before.js
@@ -8,5 +8,10 @@ app.component('prmTopBarBefore', {
         bindings: {parentCtrl: `<`},
         templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"  
 });
+
+app.component('prmSearchBarAfter', {
+    bindings: {parentCtrl: `<`},
+    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"  
+});
 	
 })();
