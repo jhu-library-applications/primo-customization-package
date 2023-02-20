9fe6a85d2a609117c3c2b10e090441566ba3ec38
little9 <jamie@jamielittle.org>
2023-02-20 14:11:12 -0600

    Adding a search result thumbnail customization

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -13,4 +13,11 @@
         bindings: { parentCtrl: `<` },
         templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
     });
+
+    app.component('prmSearchResultThumbnailContainerAfter', {
+        bindings: { parentCtrl: `<` },
+        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
+    });
+
+     
 })();
