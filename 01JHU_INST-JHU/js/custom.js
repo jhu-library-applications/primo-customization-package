bef092203259bf529c26e350719d582ddb07df78
Jamie Little <jamie@jamielittle.org>
2023-03-01 15:29:16 -0600

    Disable custom topbar

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -3,12 +3,12 @@
     'use strict';
     
     var app = angular.module('viewCustom', ['angularLoad']);
-
+/*
     app.component('prmTopBarBefore', {
         bindings: { parentCtrl: `<` },
         templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
     });
-
+*/
     app.component('prmSearchBarAfter', {
         bindings: { parentCtrl: `<` },
         templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
