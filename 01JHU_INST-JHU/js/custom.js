3f29bab22ccfa9860934543c13ac9547c05724f3
Jamie Little <jamie@jamielittle.org>
2023-07-25 09:28:31 -0500

    Update custom.js

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -1,27 +1,25 @@
-(function () {    
-    var app = angular.module('viewCustom', ['angularLoad']);
-
-    app.component('prmTopBarBefore', {
-        bindings: { parentCtrl: `<` },
-        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
-    });
-
-    app.component('prmSearchBarAfter', {
-        bindings: { parentCtrl: `<` },
-        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
-    });
-
-    app.component('prmSearchResultThumbnailContainerAfter', {
-        bindings: { parentCtrl: `<` },
-        templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
-    });
-})();
-
-document.addEventListener("DOMContentLoaded", function () {
-  const itemRequestElement = document.querySelector('span[translate="AlmaItemRequest"]');
-  const checkBorrowDirectSpan = document.querySelector('span[translate="Check BorrowDirect"]');
+(function () {
+  var app = angular.module('viewCustom', ['angularLoad']);
+  var itemRequestElement = document.querySelector('span[translate="AlmaItemRequest"]');
+  var checkBorrowDirectSpan = document.querySelector('span[translate="Check BorrowDirect"]');
 
   if (itemRequestElement) {
     checkBorrowDirectSpan.style.display = "none";
+    checkBorrowDirectSpan.parentElement.style.display = "none";
   }
-});
+
+  app.component('prmTopBarBefore', {
+    bindings: { parentCtrl: `<` },
+    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
+  });
+
+  app.component('prmSearchBarAfter', {
+    bindings: { parentCtrl: `<` },
+    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
+  });
+
+  app.component('prmSearchResultThumbnailContainerAfter', {
+    bindings: { parentCtrl: `<` },
+    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
+  });
+})();
