be84db189520f9df5ad13d38ede5ef568d60767d
Jamie Little <jamie@jamielittle.org>
2023-07-25 08:56:47 -0500

    Update custom.js
    Hide BorrowDirect if there is a request button

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -1,7 +1,4 @@
-(function () {
-    "use strict";
-    'use strict';
-    
+(function () {    
     var app = angular.module('viewCustom', ['angularLoad']);
 
     app.component('prmTopBarBefore', {
@@ -18,6 +15,13 @@
         bindings: { parentCtrl: `<` },
         templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
     });
-
-     
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const itemRequestElement = document.querySelector('span[translate="AlmaItemRequest"]');
+  const checkBorrowDirectSpan = document.querySelector('span[translate="Check BorrowDirect"]');
+
+  if (itemRequestElement) {
+    checkBorrowDirectSpan.style.display = "none";
+  }
+});
