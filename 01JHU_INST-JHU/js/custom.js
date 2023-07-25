d42998eb5ba6ab9f1cea18f528e8e26f1110112e
Jamie Little <jamie@jamielittle.org>
2023-07-25 12:54:27 -0500

    Update custom.js

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -1,6 +1,8 @@
 (function () {
 "use strict";
-
+  
+ var app = angular.module('viewCustom', ['angularLoad']);
+  
   app.component('prmTopBarBefore', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
