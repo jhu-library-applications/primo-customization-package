66eafe5a35fbdc697839c14b320c567723c0f13a
little9 <jamie@jamielittle.org>
2023-07-25 15:52:17 -0500

    Hide BD and Ill links if the item is Available

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -1,8 +1,8 @@
 (function () {
-"use strict";
-  
- var app = angular.module('viewCustom', ['angularLoad']);
-  
+  "use strict";
+
+  var app = angular.module('viewCustom', ['angularLoad']);
+
   app.component('prmTopBarBefore', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
@@ -18,15 +18,35 @@
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
   });
 
-  var observer = new MutationObserver(function() {
-    var itemRequestElement = document.querySelector('span[translate="AlmaItemRequest"]');
-    var checkBorrowDirectSpan = document.querySelector('span[translate="Check BorrowDirect"]');
+  var observer = new MutationObserver(function () {
+    // Item request information
+    var itemRequestElements = document.querySelectorAll('span[translate="AlmaItemRequest"]');
 
-    if (itemRequestElement && checkBorrowDirectSpan) {
-      checkBorrowDirectSpan.parentElement.style.display = "none"; 
+    // Our General Electronic Service links
+    var checkBorrowDirectSpans = document.querySelectorAll('span[translate="Check BorrowDirect"]');
+    var checkIlliadSpans = document.querySelectorAll('span[translate="Go to Interlibrary Loan Form"]');
+
+    // Is the item available or not? 
+    var available = document.querySelector('.availability-status').textContent.indexOf('Available') === 0
+
+    if (itemRequestElements && checkBorrowDirectSpans.length >= 1 && checkIlliadSpans.length >= 1 && available) {
+      checkBorrowDirectSpans.forEach(function (checkBorrowDirectSpan) {
+        checkBorrowDirectSpan.parentElement.style.display = "none";
+      });
+
+      checkIlliadSpans.forEach(function (checkIlliadSpan) {
+        checkIlliadSpan.parentElement.style.display = "none";
+      });
+
+      document.querySelectorAll('.skewed-divider').forEach(function (skewedDivider) {
+        skewedDivider.style.display = "none"
+      });
+
       observer.disconnect();
     }
   });
 
+
   observer.observe(document, { childList: true, subtree: true });
+
 })();
