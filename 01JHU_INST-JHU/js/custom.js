7c1e0a1aa93059c0d76842dd44d347f6d73d9db6
Jamie Little <jamie@jamielittle.org>
2023-09-06 12:40:15 -0500

    ILL link changes
    Use angular.js directly to hide/show ILL links

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -1,118 +1,72 @@
 (function () {
-  "use strict";
+  "use strict"
 
-  var app = angular.module('viewCustom', ['angularLoad']);
+  var app = angular.module('viewCustom', ['angularLoad'])
 
   app.component('prmTopBarBefore', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
-  });
+  })
 
   app.component('prmSearchBarAfter', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
-  });
+  })
 
   app.component('prmSearchResultThumbnailContainerAfter', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
-  });
+  })
+
+  app.component('prm-opac-after', {
+    bindings: { parentCtrl: `<` },
+    controller: 'prmOpacAfterController'
+  })
 
 
-  var JHU = {
-    // JHU specific customizations
-    // Add custom JS that doesn't use AngularJS here 
-    options: {
-      customUserJourneyActive: true
-    },
-    userJourney: {
-      observerActive: false,
-      startPageOverrides: function () {
-        if (!this.observerActive) {
-          // Using a mutation observer allows us to watch for changes made to the DOM
-          // This allows us to watch for changes that might be made through XHR requests
-          // or other content dynamically added to the page
-          this.observer = new MutationObserver(function () {
-            var itemRequestElements = document.querySelectorAll('span[translate="AlmaItemRequest"]');
-            var checkBorrowDirectSpans = document.querySelectorAll('span[translate="Check BorrowDirect"]');
-            var checkIlliadSpans = document.querySelectorAll('span[translate="Go to Interlibrary Loan Form"]');
-            var available = false;
+  app.controller('prmLocationItemAfter', ['$scope', function ($scope) {
+    var vm = this
+    var items = vm.$scope().$parent.$ctrl.currLoc.items
 
-            if (document.querySelector('prm-location-items .availability-status') && document.querySelector('prm-location-items .availability-status').textContent) {
-              available = document.querySelector('prm-location-items .availability-status').textContent.indexOf('Available') === 0;
-            }
+    // Disable BorrowDirect and Illiad links for items that are available 
+    items.forEach((item) => {
+      if (item._additionalData.itemstatusname === 'Item in Place' &&
+        item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') < 0
+      )
+        item.listOfServices.forEach((service) => { service.allowed = "N" })
+    })
 
-            // Hide request button if item is not available locally 
-            if (itemRequestElements && checkBorrowDirectSpans.length >= 1 && checkIlliadSpans.length >= 1 && !available) {
-              itemRequestElements.forEach(function (itemRequestElement) {
-                itemRequestElement.parentElement.style.display = "none";
-              });
+    // Enable Illiad links for Welch reading room items 
+    items.forEach((item) => {
+      if (item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') >= 0) {
+        item.listOfServices.forEach((service) => { service.allowed = "Y" })
+      }
 
-              document.querySelectorAll('.skewed-divider').forEach(function (skewedDivider) {
-                skewedDivider.style.display = "none";
-              });
+    })
+  }])
 
-              this.observer.disconnect();
-              this.observerActive = false;
-            }
+  /* StackMap: Start */
+  (function () {
 
-            // Hide BorrowDirect and Illiad links if item is available locally
-            if (itemRequestElements && checkBorrowDirectSpans.length >= 1 && checkIlliadSpans.length >= 1 && available) {
-              checkBorrowDirectSpans.forEach(function (checkBorrowDirectSpan) {
-                checkBorrowDirectSpan.parentElement.style.display = "none";
-              });
+    var a = document.querySelector("head")
+    var css1 = document.createElement("link")
+    css1.type = "text/css"
+    css1.rel = "Stylesheet"
+    css1.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    css1.crossorigin = "anonymous"
+    a.appendChild(css1)
 
-              checkIlliadSpans.forEach(function (checkIlliadSpan) {
-                checkIlliadSpan.parentElement.style.display = "none";
-              });
+    var css2 = document.createElement("link")
+    css2.type = "text/css"
+    css2.rel = "Stylesheet"
+    css2.href = "https://www.stackmapintegration.com/jhu-primo/StackMap.min.css"
+    a.appendChild(css2)
 
-              document.querySelectorAll('.skewed-divider').forEach(function (skewedDivider) {
-                skewedDivider.style.display = "none";
-              });
+    var w = document.createElement("script")
+    w.type = "text/javascript" w.async = true
+    w.src = "https://www.stackmapintegration.com/jhu-primo/StackMap.min.js"
+    var b = document.body
+    b.appendChild(w)
 
-              this.observer.disconnect();
-              this.observerActive = false;
-            }
-          }.bind(this)); // bind the context of this to the observer function
-
-          this.observer.observe(document, { childList: true, subtree: true });
-          this.observerActive = true;
-        }
-      },
-    }
-  };
-
-  if (JHU.options.customUserJourneyActive) {
-    JHU.userJourney.startPageOverrides();
-
-    document.addEventListener('click', function () {
-      JHU.userJourney.startPageOverrides();
-    });
-  }
-})();
-
-/* StackMap: Start */
-(function(){
-
-  var a = document.querySelector("head");
-  var css1 = document.createElement("link");
-  css1.type = "text/css";
-  css1.rel = "Stylesheet";
-  css1.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-  css1.crossorigin = "anonymous";
-  a.appendChild(css1);
-
-  var css2 = document.createElement("link");
-  css2.type = "text/css";
-  css2.rel = "Stylesheet";
-  css2.href = "https://www.stackmapintegration.com/jhu-primo/StackMap.min.css";
-  a.appendChild(css2);
-
-  var w = document.createElement("script");
-  w.type = "text/javascript"; w.async = true;
-  w.src = "https://www.stackmapintegration.com/jhu-primo/StackMap.min.js";
-  var b = document.body;
-  b.appendChild(w);
-
-})();
-/* StackMap: END */
+  })()
+})
