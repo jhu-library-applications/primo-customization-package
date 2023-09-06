c544a8aff210d91ea51feecfdfc4d9eea57b052d
Jamie Little <jamie@jamielittle.org>
2023-09-06 13:04:08 -0500

    logging

--- 01JHU_INST-welchill/js/custom.js
+++ 01JHU_INST-welchill/js/custom.js
@@ -28,17 +28,22 @@
     var vm = this
     var items = vm.$scope().$parent.$ctrl.currLoc.items
 
+    console.log(`Items: ${items}`)
     // Disable BorrowDirect and Illiad links for items that are available 
     items.forEach((item) => {
+      console.log(`Additional item data: ${item._additionalData}`)
       if (item._additionalData.itemstatusname === 'Item in Place' &&
         item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') < 0
-      )
+      ) {
+        console.log("non-welch")
         item.listOfServices.forEach((service) => { service.allowed = "N" })
+      }
     })
 
     // Enable Illiad links for Welch reading room items 
     items.forEach((item) => {
       if (item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') >= 0) {
+        console.log("welch")
         item.listOfServices.forEach((service) => { service.allowed = "Y" })
       }
 
