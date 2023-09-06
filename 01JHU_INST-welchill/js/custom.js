9075fa659f11773205487a974f4a268ce4967ad0
Jamie Little <jamie@jamielittle.org>
2023-09-06 15:35:10 -0500

    Detailed logging

--- 01JHU_INST-welchill/js/custom.js
+++ 01JHU_INST-welchill/js/custom.js
@@ -1,75 +1,92 @@
 (function () {
-  "use strict"
+  "use strict";
 
-  var app = angular.module('viewCustom', ['angularLoad', 'expand-items', 'request-links'])
+  console.log("Initializing 'viewCustom' module...");
+
+  var app = angular.module('viewCustom', ['angularLoad', 'expand-items']);
+
+  console.log("'viewCustom' module initialized.");
 
   app.component('prmTopBarBefore', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
-  })
+  });
+
+  console.log("Registered 'prmTopBarBefore' component.");
 
   app.component('prmSearchBarAfter', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
-  })
+  });
+
+  console.log("Registered 'prmSearchBarAfter' component.");
 
   app.component('prmSearchResultThumbnailContainerAfter', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
-  })
+  });
 
+  console.log("Registered 'prmSearchResultThumbnailContainerAfter' component.");
 
   angular
     .module('expand-items', [])
     .component('prmLocationItemAfter', {
       bindings: { parentCtrl: '<' },
-      controller: function ($timeout) {
-        var ctrl = this
+      controller: function () {
+        var ctrl = this;
         this.$onInit = function () {
-          {
-            $timeout(showDetails, 2000)
-            function showDetails() {
-              ctrl.parentCtrl.loc.isExpandAll = true
-              ctrl.parentCtrl.currLoc.items.forEach((item) => {
-                item.isExpanded = true
-              })
+          console.log("Inside $onInit for 'prmLocationItemAfter' component.");
+
+          function showDetails() {
+            console.log("Executing showDetails...");
+            
+            if (!ctrl.parentCtrl.loc || !ctrl.parentCtrl.currLoc.items) {
+              console.log("Required properties are not available. Exiting showDetails.");
+              return;
             }
+
+            ctrl.parentCtrl.loc.isExpandAll = true;
+            ctrl.parentCtrl.currLoc.items.forEach((item) => {
+              item.isExpanded = true;
+            });
+            
+            console.log("showDetails executed successfully.");
           }
+
+          function requestLinks() {
+            console.log("Executing requestLinks...");
+
+            if (!ctrl.parentCtrl.currLoc.items) {
+              console.log("Required properties are not available. Exiting requestLinks.");
+              return;
+            }
+
+            var items = ctrl.parentCtrl.currLoc.items;
+            items.forEach((item) => {
+              console.log(`Additional item data: ${item._additionalData}`);
+              if (item._additionalData.itemstatusname === 'Item in Place' &&
+                  item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') < 0) {
+                console.log("non-welch");
+                item.listOfServices.forEach((service) => { service.allowed = "N"; });
+              }
+            });
+
+            items.forEach((item) => {
+              if (item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') >= 0) {
+                console.log("welch");
+                item.listOfServices.forEach((service) => { service.allowed = "Y"; });
+              }
+            });
+
+            console.log("requestLinks executed successfully.");
+          }
+
+          showDetails();
+          requestLinks();
+          console.log("Exiting $onInit for 'prmLocationItemAfter' component.");
         }
       }
-    })
+    });
 
-
-  angular
-    .module('request-links', [])
-    .component('prmLocationItemAfter', {
-      bindings: { parentCtrl: '<' },
-      controller: function ($timeout) {
-        var ctrl = this
-        this.$onInit = function () {
-          {
-            $timeout(requestLinks, 2000)
-            function requestLinks() {
-              var items = ctrl.parentCtrl.currLoc.items
-              items.forEach((item) => {
-                console.log(`Additional item data: ${item._additionalData}`)
-                if (item._additionalData.itemstatusname === 'Item in Place' &&
-                  item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') < 0
-                ) {
-                  console.log("non-welch")
-                  item.listOfServices.forEach((service) => { service.allowed = "N" })
-                }
-              })
-
-              items.forEach((item) => {
-                if (item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') >= 0) {
-                  console.log("welch")
-                  item.listOfServices.forEach((service) => { service.allowed = "Y" })
-                }
-              })
-            }
-          }
-        }
-      }
-    })
-})
+  console.log("Registered 'prmLocationItemAfter' component in 'expand-items' module.");
+})();
