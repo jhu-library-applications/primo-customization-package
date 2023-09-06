524e4cd0f41cbbbc34c4dbe5bd09db6499133c8a
Jamie Little <jamie@jamielittle.org>
2023-09-06 15:18:00 -0500

    modules

--- 01JHU_INST-welchill/js/custom.js
+++ 01JHU_INST-welchill/js/custom.js
@@ -18,35 +18,58 @@
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
   })
 
-  app.component('prmLocationItemAfter', {
-    bindings: { parentCtrl: `<` },
-    controller: 'prmLocationItemAfterController'
-  })
 
-
-  app.controller('prmLocationItemAfterController', ['$scope', function ($scope) {
-    var vm = this
-    var items = vm.$scope().$parent.$ctrl.currLoc.items
-
-    console.log(`Items: ${items}`)
-    // Disable BorrowDirect and Illiad links for items that are available 
-    items.forEach((item) => {
-      console.log(`Additional item data: ${item._additionalData}`)
-      if (item._additionalData.itemstatusname === 'Item in Place' &&
-        item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') < 0
-      ) {
-        console.log("non-welch")
-        item.listOfServices.forEach((service) => { service.allowed = "N" })
+  angular
+    .module('expand-items', [])
+    .component('prmLocationItemAfter', {
+      bindings: { parentCtrl: '<' },
+      controller: function ($timeout) {
+        var ctrl = this
+        this.$onInit = function () {
+          {
+            $timeout(showDetails, 2000)
+            function showDetails() {
+              ctrl.parentCtrl.loc.isExpandAll = true
+              ctrl.parentCtrl.currLoc.items.forEach((item) => {
+                item.isExpanded = true
+              })
+            }
+          }
+        }
       }
     })
 
-    // Enable Illiad links for Welch reading room items 
-    items.forEach((item) => {
-      if (item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') >= 0) {
-        console.log("welch")
-        item.listOfServices.forEach((service) => { service.allowed = "Y" })
-      }
 
+  angular
+    .module('request-links', [])
+    .component('prmLocationItemAfter', {
+      bindings: { parentCtrl: '<' },
+      controller: function ($timeout) {
+        var ctrl = this
+        this.$onInit = function () {
+          {
+            $timeout(requestLinks, 2000)
+            function requestLinks() {
+              var items = ctrl.parentCtrl.currLoc.items
+              items.forEach((item) => {
+                console.log(`Additional item data: ${item._additionalData}`)
+                if (item._additionalData.itemstatusname === 'Item in Place' &&
+                  item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') < 0
+                ) {
+                  console.log("non-welch")
+                  item.listOfServices.forEach((service) => { service.allowed = "N" })
+                }
+              })
+
+              items.forEach((item) => {
+                if (item._additionalData.secondarylocationname.indexOf('Onsite Print Use Only') >= 0) {
+                  console.log("welch")
+                  item.listOfServices.forEach((service) => { service.allowed = "Y" })
+                }
+              })
+            }
+          }
+        }
+      }
     })
-  }])
 })
