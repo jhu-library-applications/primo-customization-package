95e210c5127e8d4a08efe0ef71826c41cac7df8d
Jamie Little <jamie@jamielittle.org>
2024-02-21 07:22:47 -0600

    Remove prmSearchBarAfter component
    This component isn't being used and was just a placeholder. Let's get rid of it!

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -24,11 +24,6 @@
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
   });
 
-  app.component('prmSearchBarAfter', {
-    bindings: { parentCtrl: `<` },
-    templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-bar-after.html"
-  });
-
   app.component('prmSearchResultThumbnailContainerAfter', {
     bindings: { parentCtrl: `<` },
     templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-search-result-thumbnail-container-after.html"
