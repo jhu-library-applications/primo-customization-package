4e5d14b797a9f1e23aac1d3a0ed7f4e9ba558b70
Jamie Little <jamie@jamielittle.org>
2024-06-06 14:11:30 -0500

    Catch array access errors

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -7,42 +7,53 @@
     $httpProvider.interceptors.push('itemRequestUrlInterceptor');
   }]);
 
-  app.factory('itemRequestUrlInterceptor', function () {
+  app.factory('itemRequestUrlInterceptor', function() {
     return {
-      request: function (config) {
-        if (config.url.includes('AlmaItemRequest')) {
-          console.log(config)
-        }
-        return config;
-      },
-      response: function (response) {
-        if (response.config.url.includes('AlmaItemRequest')) {
-          response.data['services-arr'].services.forEach(function (service) {
-            service['groups-list-map'].forEach(function (item) {
-              item.pickupLocation.forEach(function (location) {
-                if (location.key === "126006350007861$$LIBRARY") {
-                  location.value = "Milton S. Eisenhower Library - Annex";
+        request: function(config) {
+            if (config.url.includes('AlmaItemRequest')) {
+                console.log('Request Config:', config);
+            }
+            return config;
+        },
+        response: function(response) {
+            if (response.config.url.includes('AlmaItemRequest')) {
+                try {
+                    response.data['services-arr'].services.forEach(function(service) {
+                        service['groups-list-map'].forEach(function(item) {
+                            item.pickupLocation.forEach(function(location) {
+                                if (location.key === "126006350007861$$LIBRARY") {
+                                    location.value = "Milton S. Eisenhower Library - Annex";
+                                }
+                            });
+                        });
+                    });
+                    console.log('Modified Response:', response.data);
+                } catch (error) {
+                    console.log('Error modifying response:', error);
+                    return response;
                 }
-              });
-            });
-          });
+            }
+            return response;
+        },
+        requestError: function(rejection) {
+            if (rejection.config && rejection.config.url.includes('AlmaItemRequest')) {
+                console.log('Request Error:', rejection);
+            }
+            return Promise.reject(rejection);
+        },
+        responseError: function(rejection) {
+            if (rejection.config && rejection.config.url.includes('AlmaItemRequest')) {
+                console.log('Response Error:', rejection);
+            }
+            return Promise.reject(rejection);
         }
-        return response;
-      },
-      requestError: function (rejection) {
-        if (rejection.config && rejection.config.url.includes('AlmaItemRequest')) {
-          console.log('Request Error: ', rejection)
-        }
-        return Promise.reject(rejection);
-      },
-      responseError: function (rejection) {
-        if (rejection.config && rejection.config.url.includes('AlmaItemRequest')) {
-          console.log('Response Error: ', rejection)
-        }
-        return Promise.reject(rejection);
-      }
     };
-  });
+});
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('itemRequestUrlInterceptor');
+}]);
+
 
   /* Components */
   app.component('prmTopBarBefore', {
