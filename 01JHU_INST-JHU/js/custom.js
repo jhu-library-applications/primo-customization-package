5faa653fa2c7bc1c49872313fec93808f39fbefa
Jamie Little <jamie@jamielittle.org>
2024-06-11 08:55:09 -0500

    Eisenhower notice (#10)
    * Fix injection
    
    * Check the patron's group
    
    This checks the patron's group to determing if they are eligible for office delivery,
    
    * Add a pickup notice for the MSEL dropdown
    
    If a patron selects MSEL as a pickup location a notice will display.
    
    * Test overriding the library name
    
    This changes the library name with an angularjs
    interceptor.
    
    * Catch array access errors
    
    * Remove library name change
    
    * No name override
    
    * No name override
    
    * Homepage fix

--- 01JHU_INST-JHU/js/custom.js
+++ 01JHU_INST-JHU/js/custom.js
@@ -3,6 +3,64 @@
 
   var app = angular.module('viewCustom', ['angularLoad']);
 
+
+
+  /* Library name override */
+  /* This is used to change the name of the library in the request form */
+  /*
+
+  app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('itemRequestUrlInterceptor');
+  }]);
+  
+  app.factory('itemRequestUrlInterceptor', function() {
+    return {
+        request: function(config) {
+            if (config.url.includes('AlmaItemRequest')) {
+                console.log('Request Config:', config);
+            }
+            return config;
+        },
+        response: function(response) {
+            if (response.config.url.includes('AlmaItemRequest')) {
+                try {
+                    response.data['services-arr'].services.forEach(function(service) {
+                        service['groups-list-map'].forEach(function(item) {
+                            item.pickupLocation.forEach(function(location) {
+                                if (location.key === "126006350007861$$LIBRARY") {
+                                    location.value = "Milton S. Eisenhower Library - Annex";
+                                }
+                            });
+                        });
+                    });
+                    console.log('Modified Response:', response.data);
+                } catch (error) {
+                    console.log('Error modifying response:', error);
+                    return response;
+                }
+            }
+            return response;
+        },
+        requestError: function(rejection) {
+            if (rejection.config && rejection.config.url.includes('AlmaItemRequest')) {
+                console.log('Request Error:', rejection);
+            }
+            return Promise.reject(rejection);
+        },
+        responseError: function(rejection) {
+            if (rejection.config && rejection.config.url.includes('AlmaItemRequest')) {
+                console.log('Response Error:', rejection);
+            }
+            return Promise.reject(rejection);
+        }
+    };
+});
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('itemRequestUrlInterceptor');
+}]);
+
+
   /* Components */
   app.component('prmTopBarBefore', {
     bindings: { parentCtrl: `<` },
@@ -63,28 +121,74 @@
 
   app.component('prmRequestAfter', {
     bindings: { parentCtrl: '<' },
-    template: '<div></div>',
-    controller: ['primawsRest', '$scope', function ($scope) {
+    template: `<div><style>#form_field_genericCheckBox { display: none; } #pickup-notice { display: none; }  </style>
+    <div id="pickup-notice">
+    <md-card md-theme="{{ showDarkTheme ? 'dark-grey' : 'default' }}" md-theme-watch>
+    <md-card-title>
+      <md-card-title-text>
+        <span class="md-headline">Attention</span>
+      </md-card-title-text>
+    </md-card-title>
+    <md-card-content>
+    The Eisenhower Pick Up Shelf will be unavailable beginning June 18th. Pick Ups will resume at the MSE Library Annex (The building formerly known as The Hopkins Club) on the week of June 24th.
+    </md-card-content>
+  </md-card>
+    
+    </div>`,
+    controller: ['$scope', 'primawsRest', function ($scope, primawsRest) {
+      var patronStatusCode = "";
 
       this.$onInit = function () {
+
+        primawsRest.myAccountPersonalSettings().then(function successCallback(response) {
+          console.log(response.data);
+          patronStatusCode = response.data.data.patronstatus[0].registration[0].institution[0].patronstatuscode;
+        }, function errorCallback(response) {
+          //Failure
+          console.log(response);
+        });
 
         // Watch for changes in the dropdown value
         $scope.$watch(() => this.parentCtrl.formData["pickupLocation"], (newValue, oldValue) => {
           if (newValue !== oldValue) { // Check if the value has actually changed
-            this.updateCheckboxVisibility(newValue);
+            console.log(newValue);
+            pickupNotice(newValue);
           }
         });
       };
 
-      this.updateCheckboxVisibility = function (selectedLocationId) {
+
+      function pickupNotice(selectedLocationId) {
+        const eisenhowerId = "126006350007861$$LIBRARY";
+        const pickupNotice = document.getElementById('pickup-notice');
+
+        if (selectedLocationId === eisenhowerId) {
+          pickupNotice.style.display = 'block';
+        } else {
+          pickupNotice.style.display = 'none';
+        }
+
+      }
+      function campusDeliveryEligible(patronStatusCode, selectedLocationId) {
         const homewoodId = "126006350007861$$LIBRARY";
         const welchId = "126007910007861$$LIBRARY";
+        const eligibleHomewoodGroups = ["jhfac", "jhgrad"];
+        const eligibleWelchGroups = ["jhfac"];
 
+        if (selectedLocationId === homewoodId) {
+          return eligibleHomewoodGroups.includes(patronStatusCode);
+        }
+
+        if (selectedLocationId === welchId) {
+          return eligibleWelchGroups.includes(patronStatusCode);
+        }
+      }
+
+      this.updateCheckboxVisibility = function (selectedLocationId) {
         const checkbox = document.getElementById('form_field_genericCheckBox');
 
-        if (checkbox != null && (selectedLocationId === homewoodId || selectedLocationId === welchId)) {
+        if (campusDeliveryEligible(patronStatusCode, selectedLocationId)) {
           checkbox.style.display = 'block';
-          console.log(selectedLocationId === homewoodId ? "Homewood selected" : "Welch selected");
         } else {
           checkbox.style.display = 'none';
         }
@@ -102,7 +206,7 @@
     }
   });
 
-  /* This service is used to get the patron/user's info -- may not be needed, but could be handy */
+  /* This service is used to get the patron/user's info -- currently used for office delivery */
   app.service("primawsRest", ['$http', function ($http) {
     this.myAccountPersonalSettings = function () {
       return $http({
