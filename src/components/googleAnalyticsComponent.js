5ab988a3e16797961aa3b0b8f255b5ae06090ef2
Jamie Little <jamie@jamielittle.org>
2024-07-16 08:07:28 -0500

    Add Google Analytics component

---
+++ src/components/googleAnalyticsComponent.js
@@ -0,0 +1,30 @@
+export const googleAnalyticsComponent = {
+    template: '<div></div>',
+    controller: class GoogleAnalyticsController {
+      constructor($window) {
+        this.$window = $window;
+      }
+  
+      $onInit() {
+        this.insertGoogleAnalytics();
+      }
+  
+      insertGoogleAnalytics() {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = 'https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID'; // Replace GA_MEASUREMENT_ID with your actual ID
+        document.head.appendChild(script);
+  
+        this.$window.dataLayer = this.$window.dataLayer || [];
+        function gtag() {
+          this.$window.dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'GTM-KVWVGLP');
+      }
+    },
+    controllerAs: 'vm'
+  };
+  
+  GoogleAnalyticsController.$inject = ['$window'];
+  
