c8157fd99af6492ba2274167975924c4583152f0
Jamie Little <jamie@jamielittle.org>
2024-07-17 10:32:46 -0500

    GA Updates

--- src/components/googleAnalyticsComponent.js
+++ src/components/googleAnalyticsComponent.js
@@ -1,30 +1,30 @@
 export const googleAnalyticsComponent = {
-    template: '<div></div>',
-    controller: class GoogleAnalyticsController {
-      constructor($window) {
-        this.$window = $window;
-      }
-  
-      $onInit() {
-        this.insertGoogleAnalytics();
-      }
-  
-      insertGoogleAnalytics() {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = 'https://www.googletagmanager.com/gtag/js?id=GTM-KVWVGLP'; // Replace GA_MEASUREMENT_ID with your actual ID
-        document.head.appendChild(script);
-  
-        this.$window.dataLayer = this.$window.dataLayer || [];
-        function gtag() {
-          this.$window.dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'GTM-KVWVGLP');
-      }
-    },
-    controllerAs: 'vm'
-  };
-  
-  GoogleAnalyticsController.$inject = ['$window'];
-  
+  template: '<div></div>',
+  controller: class GoogleAnalyticsController {
+    static $inject = ['$window'];
+
+    constructor($window) {
+      this.$window = $window;
+    }
+
+    $onInit() {
+      this.insertGoogleAnalytics();
+    }
+
+    insertGoogleAnalytics() {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=GTM-KVWVGLP'; // Replace GTM-KVWVGLP with your actual ID
+      document.head.appendChild(script);
+
+      this.$window.dataLayer = this.$window.dataLayer || [];
+      const gtag = (...args) => {
+        this.$window.dataLayer.push(args);
+      };
+
+      gtag('js', new Date());
+      gtag('config', 'GTM-KVWVGLP');
+    }
+  },
+  controllerAs: 'vm'
+};
