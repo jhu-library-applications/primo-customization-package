c7f4871b557d33133671b6f61acb86552e0ab3cb
Jamie Little <jamie@jamielittle.org>
2024-11-08 15:41:30 -0600

    Add eucookie popup survey link

--- build.js
+++ build.js
@@ -1,8 +1,10 @@
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 
-esbuild.build({
-  entryPoints: ['src/app.js'],
-  bundle: true,
-  external: ['angular', 'angularLoad'], // Mark angular and angularLoad as external
-  outfile: '01JHU_INST-JHU/js/custom.js', // Output file
-}).catch(() => process.exit(1));
+esbuild
+  .build({
+    entryPoints: ["src/app.js"],
+    bundle: true,
+    external: ["angular", "angularLoad"], // Mark angular and angularLoad as external
+    outfile: "01JHU_INST-JHU/js/custom.js", // Output file
+  })
+  .catch(() => process.exit(1));
--- src/components/prmTopBarBefore.js
+++ src/components/prmTopBarBefore.js
@@ -1,4 +1,4 @@
 export const prmTopBarBefore = {
   bindings: { parentCtrl: `<` },
-  templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html"
+  templateUrl: "/discovery/custom/01JHU_INST-JHU/html/prm-top-bar-before.html",
 };
